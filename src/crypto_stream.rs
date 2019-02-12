f7a76d18ffa57687d4d28116cfeed2af1e712143
timvisee <tim@visee.me>
2019-02-12 15:01:42 +0100

    Define pipe traits, similar to generic crypto traits

--- src/crypto_stream.rs
+++ src/crypto_stream.rs
@@ -561,6 +561,64 @@ impl Write for EceWriter {
     }
 }
 
+/// Something that can pipe given data.
+///
+/// A pipe may monitor, but also transform data flowing through it.
+///
+/// From this pipe, a reader or writer can be created using the corresponding `reader` and `writer`
+/// functions to wrap an existing reader or writer.
+pub trait Pipe: Sized {
+    /// The wrapping reader type used for this pipe.
+    type Reader: PipeRead<Self>;
+
+    /// The wrapping writer type used for this pipe.
+    type Writer: PipeWrite<Self>;
+
+    /// Wrap the `inner` reader, bytes that are read are transformed through this pipe.
+    fn reader(self, inner: Box<dyn Read>) -> Self::Reader {
+        Self::Reader::new(self, inner)
+    }
+
+    /// Wrap the `inner` writer, bytes that are read are transformed through this pipe.
+    fn writer(self, inner: Box<dyn Write>) -> Self::Writer {
+        Self::Writer::new(self, inner)
+    }
+
+    /// Pipe bytes from `input`, monitor/transform it, return the output.
+    ///
+    /// This returns a tuple with the number of bytes read from the `input`, along with transformed
+    /// data if available in the following format: `(read_bytes, transformed_data)`.
+    ///
+    /// Pipes that just monitor data, immediately return the input.
+    /// The `pipe_transparent` helper function can be used for this inside the implementation.
+    fn pipe(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>);
+
+    /// Transparently pipe `input` by immediately returning it.
+    ///
+    /// This can be used as helper function inside the `pipe` function if data is only monitored
+    /// and not transformed.
+    #[inline]
+    fn pipe_transparent(&self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
+        (input.len(), Some(input.to_vec()))
+    }
+}
+
+/// A reader wrapping another reader, to encrypt or decrypt data read from it.
+pub trait PipeRead<P>: Read
+    where P: Pipe,
+{
+    /// Wrap the given `inner` reader, transform data using `crypt`.
+    fn new(pipe: P, inner: Box<dyn Read>) -> Self;
+}
+
+/// A writer wrapping another writher, to encrypt or decrypt data it is writen to.
+pub trait PipeWrite<P>: Write
+    where P: Pipe,
+{
+    /// Wrap the given `inner` writer, transform data using `crypt`.
+    fn new(pipe: P, inner: Box<dyn Write>) -> Self;
+}
+
 /// Defines number of bytes transformed into/out of pipe if constant and known.
 ///
 /// For example, the AES-GCM cryptography pipe adds 16 bytes to the encrypted payload for the
