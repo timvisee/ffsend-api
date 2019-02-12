f6c170e64b41796db6f087a89503155628277bfb
timvisee <tim@visee.me>
2019-02-12 15:48:34 +0100

    Extract pipe and crypto trait definitions from module root into own

--- src/pipe/crypto/mod.rs
+++ src/pipe/crypto/mod.rs
@@ -1,52 +1,13 @@
-use openssl::symm::Mode as OpenSslMode;
-
-use crate::pipe::Pipe;
-
+mod crypt;
 mod ece;
 mod gcm;
+
+// Re-export modules
+pub use crypt::{Crypt, CryptMode};
+pub use self::ece::{EceCrypt, EceReader, EceWriter};
+pub use gcm::{GcmCrypt, GcmReader, GcmWriter};
 
 // Re-export common traits for prelude
 pub mod prelude {
     pub use super::Crypt;
-}
-
-// Re-export modules
-pub use gcm::{
-    GcmCrypt,
-    GcmReader,
-    GcmWriter,
-};
-
-/// The cryptographic mode for a crypter: encrypt or decrypt.
-#[derive(Debug, Clone, Copy)]
-pub enum CryptMode {
-    /// Encrypt data while transforming.
-    Encrypt,
-
-    /// Decrypt data while transforming.
-    Decrypt,
-}
-
-impl Into<OpenSslMode> for CryptMode {
-    fn into(self) -> OpenSslMode {
-        match self {
-            CryptMode::Encrypt => OpenSslMode::Encrypt,
-            CryptMode::Decrypt => OpenSslMode::Decrypt,
-        }
-    }
-}
-
-/// Pipe specialisation trait for cryptographic pipes.
-pub trait Crypt: Pipe {
-    /// Encrypt/decrypt bytes from `input`, return the result.
-    ///
-    /// Read bytes from the given input buffer, transform it using the configured cryptography and
-    /// return transformed data when available.
-    ///
-    /// This returns a tuple with the number of bytes read from the `input`, along with transformed
-    /// data if available in the following format: `(read_bytes, transformed_data)`.
-    #[inline(always)]
-    fn crypt(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
-        self.pipe(input)
-    }
 }
--- src/pipe/mod.rs
+++ src/pipe/mod.rs
@@ -1,6 +1,8 @@
-use std::io::{Read, Write};
-
 pub mod crypto;
+mod pipe;
+
+// Re-export modules
+pub use pipe::{Pipe, PipeRead, PipeWrite, PipeLen};
 
 // Re-export common traits for prelude
 pub mod prelude {
@@ -15,95 +17,3 @@ pub mod prelude {
 
 /// The default size of byte buffers.
 const DEFAULT_BUF_SIZE: usize = 8 * 1024;
-
-/// Something that can pipe given data.
-///
-/// A pipe may monitor, but also transform data flowing through it.
-///
-/// From this pipe, a reader or writer can be created using the corresponding `reader` and `writer`
-/// functions to wrap an existing reader or writer.
-pub trait Pipe: Sized {
-    /// The wrapping reader type used for this pipe.
-    type Reader: PipeRead<Self>;
-
-    /// The wrapping writer type used for this pipe.
-    type Writer: PipeWrite<Self>;
-
-    /// Wrap the `inner` reader, bytes that are read are transformed through this pipe.
-    fn reader(self, inner: Box<dyn Read>) -> Self::Reader {
-        Self::Reader::new(self, inner)
-    }
-
-    /// Wrap the `inner` writer, bytes that are read are transformed through this pipe.
-    fn writer(self, inner: Box<dyn Write>) -> Self::Writer {
-        Self::Writer::new(self, inner)
-    }
-
-    /// Pipe bytes from `input`, monitor/transform it, return the output.
-    ///
-    /// This should not be used directly, and is automatically used by a `PipeRead` or `PipeWrite`
-    /// object for the actual piping.
-    ///
-    /// This returns a tuple with the number of bytes read from the `input`, along with transformed
-    /// data if available in the following format: `(read_bytes, transformed_data)`.
-    ///
-    /// Pipes that just monitor data, immediately return the input.
-    /// The `pipe_transparent` helper function can be used for this inside the implementation.
-    fn pipe(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>);
-
-    /// Transparently pipe `input` by immediately returning it.
-    ///
-    /// This should not be used directly, and is automatically used by a `PipeRead` or `PipeWrite`
-    /// object for the actual piping.
-    ///
-    /// This can be used as helper function inside the `pipe` function if data is only monitored
-    /// and not transformed.
-    #[inline(always)]
-    fn pipe_transparent(&self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
-        (input.len(), Some(input.to_vec()))
-    }
-}
-
-/// A reader wrapping another reader, to encrypt or decrypt data read from it.
-pub trait PipeRead<P>: Read
-    where P: Pipe,
-{
-    /// Wrap the given `inner` reader, transform data using `crypt`.
-    fn new(pipe: P, inner: Box<dyn Read>) -> Self;
-}
-
-/// A writer wrapping another writher, to encrypt or decrypt data it is writen to.
-pub trait PipeWrite<P>: Write
-    where P: Pipe,
-{
-    /// Wrap the given `inner` writer, transform data using `crypt`.
-    fn new(pipe: P, inner: Box<dyn Write>) -> Self;
-}
-
-/// Defines number of bytes transformed into/out of pipe if fixed.
-///
-/// For example, the AES-GCM cryptography pipe adds 16 bytes to the encrypted payload for the
-/// verification tag. When transforming a file the length is known.
-/// Then, the `len_out` for an AES-GCM encrypter is 16 bytes larger than `len_in`.
-/// For a decrypter this is the other way around.
-pub trait PipeLen {
-    /// The number of bytes that are transfered into the pipe if fixed.
-    fn len_in(&self) -> usize;
-
-    /// The number of bytes that are transfered out of the pipe if fixed.
-    fn len_out(&self) -> usize;
-}
-
-// TODO: use automatic implementation for crypt reader and writer wrapping crypt having pipelen
-// impl<R, C> PipeLen for R
-//     where R: CryptRead<C>,
-//           C: Crypt + PipeLen,
-// {
-//     fn len_in(&self) -> usize {
-//         self.crypt.len_in()
-//     }
-//
-//     fn len_out(&self) -> usize {
-//         self.crypt.len_out()
-//     }
-// }
