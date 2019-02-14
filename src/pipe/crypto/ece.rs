7f7db6ae64a6cbea8401208d15e1e27b89884fb4
timvisee <tim@visee.me>
2019-02-14 18:47:40 +0100

    Start on ECE decrypting [WIP]

--- src/pipe/crypto/ece.rs
+++ src/pipe/crypto/ece.rs
@@ -3,59 +3,241 @@
 use std::io::{self, Read, Write};
 use std::cmp::min;
 
-use bytes::{BufMut, BytesMut};
-use ece::Aes128GcmEceWebPush;
+use byteorder::{BigEndian, ByteOrder};
+use bytes::{BufMut, Bytes, BytesMut};
+use openssl::symm;
 
+use crate::crypto::hkdf::hkdf;
 use crate::pipe::{
     DEFAULT_BUF_SIZE,
     prelude::*,
 };
 use super::{Crypt, CryptMode};
 
-// TODO: specify this somewhere else
-const RS: usize = 1024 * 64;
+// TODO: specify this somewhere else?
+const RS: u32 = 1024 * 64;
+
+const SALT_LENGTH: usize = 16;
+const KEY_LENGTH: usize = 16;
+const RS_LENGTH: usize = 4;
+
+// Defined by ECE standard
+const ECE_AES_KEY_LENGTH: usize = 16;
+const ECE_NONCE_LENGTH: usize = 12;
+
+const ECE_AES128GCM_HEADER_LENGTH: u32 = 21;
+const ECE_AES128GCM_KEY_INFO: &str = "Content-Encoding: aes128gcm\0";
+const ECE_AES128GCM_NONCE_INFO: &str = "Content-Encoding: nonce\0";
 
 /// Something that can encrypt or decrypt given data using ECE.
 pub struct EceCrypt {
     mode: CryptMode,
 
-    /// Chunk sequence.
-    seq: usize,
+    ikm: Vec<u8>,
+    key: Option<Vec<u8>>,
+    // This is the base nonce
+    nonce: Option<Vec<u8>>,
+
+    salt: Option<Vec<u8>>,
+
+    /// Chunk sequence, limited to `u32::MAX`.
+    seq: u32,
 
     /// The total size in bytes of the plain text payload, excluding any encryption overhead.
     len: usize,
 
+    /// The used ECE cryptographic record size.
+    rs: u32,
+
     // TODO: remove this buffer, obsolete?
-    /// Input buffer.
-    buf: BytesMut,
+    // /// Input buffer.
+    // buf: BytesMut,
 }
 
 impl EceCrypt {
     /// Construct a new ECE crypter pipe.
-    pub fn new(mode: CryptMode, len: usize, input_size: usize) -> Self {
+    pub fn new(mode: CryptMode, ikm: Vec<u8>, len: usize) -> Self {
         Self {
             mode,
+            ikm,
+            // TODO: define key
+            key: None,
+            // TODO: define nonce
+            nonce: None,
+            // TODO: define salt
+            salt: None,
             seq: 0,
             len,
-            buf: BytesMut::with_capacity(input_size),
+            rs: RS,
+            // buf: BytesMut::with_capacity(DEFAULT_BUF_SIZE),
         }
     }
+
+    // TODO: create encrypt and decrypt specific constructor, like in the AES-GCM pipe
 
     /// Get the size of the payload chunk.
     ///
     /// Data passed to the crypter must match the chunk size.
     ///
     /// The chunk size might change during encryption/decryption due to prefixed data.
-    fn chunk_size(&self) -> usize {
+    fn chunk_size(&self) -> u32 {
         match self.mode {
-            CryptMode::Encrypt => RS - 17,
-            // TODO: shouldn't this be `21`?
+            CryptMode::Encrypt => self.rs - 17,
+            // TODO: shouldn't this be `21`? Use `ECE_AES128GCM_HEADER_LENGTH` instead?
             CryptMode::Decrypt => if self.seq == 0 {
                     21
                 } else {
-                    RS
+                    self.rs
                 },
         }
+    }
+
+    /// Encrypt the given `input` data using this configured crypter.
+    ///
+    /// This function returns `(read, out)` where `read` represents the number of read bytes from
+    /// `input`, and `out` is a vector of now encrypted bytes.
+    ///
+    /// # Panics
+    ///
+    /// Panics if attempted to write more bytes than the length specified while configuring the
+    /// crypter.
+    fn pipe_encrypt(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
+        // // Don't allow encrypting more than specified, when tag is obtained
+        // if self.has_tag() && !input.is_empty() {
+        //     panic!("could not write to AES-GCM encrypter, exceeding specified length");
+        // }
+
+        // // Find input length and block size, increase current bytes counter
+        // let len = input.len();
+        // let block_size = self.cipher.block_size();
+        // self.cur += len;
+
+        // // Transform input data through crypter, collect output
+        // // TODO: do not unwrap here, but try error
+        // let mut out = vec![0u8; len + block_size];
+        // let out_len = self.crypter.update(&input, &mut out)
+        //     .expect("failed to update AES-GCM encrypter with new data");
+        // out.truncate(out_len);
+
+        // // Finalize the crypter when all data is encrypted, append finalized to output
+        // // TODO: do not unwrap in here, but try error
+        // if self.cur >= self.len && !self.has_tag() {
+        //     // Allocate tag space
+        //     self.tag = vec![0u8; GCM_TAG_SIZE];
+
+        //     let mut out_final = vec![0u8; block_size];
+        //     let final_len = self.crypter.finalize(&mut out_final)
+        //         .expect("failed to finalize AES-GCM encrypter");
+        //     out.extend_from_slice(&out_final[..final_len]);
+        //     self.crypter.get_tag(&mut self.tag)
+        //         .expect("failed to get AES-GCM encrypter tag for validation");
+
+        //     // Append tag to output
+        //     out.extend_from_slice(&self.tag);
+        // }
+
+        // (len, Some(out))
+
+        panic!("not yet implemented");
+    }
+
+    /// Decrypt the given `input` payload using this configured crypter.
+    ///
+    /// This function returns `(read, out)` where `read` represents the number of read bytes from
+    /// `input`, and `out` is a vector of now encrypted bytes.
+    ///
+    /// # Panics
+    ///
+    /// Panics if attempted to write more bytes than the length specified while configuring the
+    /// crypter.
+    fn pipe_decrypt(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
+        // // Don't allow decrypting more than specified, when tag is obtained
+        // if self.has_tag() && !input.is_empty() {
+        //     panic!("could not write to AES-GCM decrypter, exceeding specified lenght");
+        // }
+
+        // Parse the first chunk as header data
+        if self.seq == 0 {
+            self.pipe_decrypt_header(input);
+            return (input.len(), None);
+            // TODO: increase len?
+        }
+
+        // Generate the decryption nonce
+        let nonce = self.generate_nonce(self.seq);
+
+        // Decrypt the chunk
+        let out = symm::decrypt_aead(
+            symm::Cipher::aes_128_gcm(),
+            self.key.as_ref().expect("no key"),
+            Some(&nonce),
+            &[], // TODO: aad?
+            input,
+            &[], // TODO: tag?
+        ).expect("failed to decrypt ECE chunk");
+
+        // TODO: do unpadding
+
+        (input.len(), Some(out))
+    }
+
+    // TODO: actually return the header
+    fn pipe_decrypt_header(&mut self, input: &[u8]) {
+        // Assert the header chunk size
+        // TODO: is bigger than 21 because of key size
+        assert!(
+            input.len() as u32 >= ECE_AES128GCM_HEADER_LENGTH,
+            "failed to decrypt, ECE header is too short",
+        );
+
+        // Easily handle input data as bytes
+        let mut input = Bytes::from(input);
+
+        // Parse the salt, record size and length
+        self.salt = Some(input.split_to(SALT_LENGTH).to_vec());
+        self.rs = BigEndian::read_u32(&input.split_to(RS_LENGTH));
+        let key_id_len = input.split_to(1)[0] as usize;
+
+        let length = key_id_len + KEY_LENGTH + 5;
+
+        // Derive the key and nonce
+        self.key = Some(hkdf(
+            self.salt.as_ref().map(|s| s.as_slice()),
+            ECE_AES_KEY_LENGTH,
+            &self.ikm,
+            Some(ECE_AES128GCM_KEY_INFO.as_bytes()),
+        ));
+        self.nonce = Some(hkdf(
+            self.salt.as_ref().map(|s| s.as_slice()),
+            ECE_NONCE_LENGTH,
+            &self.ikm,
+            Some(ECE_AES128GCM_NONCE_INFO.as_bytes()),
+        ));
+
+        // TODO: remove after debugging
+        dbg!(self.rs);
+        dbg!(key_id_len);
+        dbg!(length);
+
+        // Assert all header bytes have been consumed
+        // TODO: Not valid? Header may be longer?
+        assert!(input.is_empty(), "failed to decrypt, not all ECE header bytes are used");
+    }
+
+    fn generate_nonce(&self, seq: u32) -> Vec<u8> {
+        // Get the base nonce which we need to modify
+        let mut nonce = self.nonce.clone().expect("failed to generate nonce, no base nonce available");
+
+        let nonce_len = nonce.len();
+        let m = BigEndian::read_u32(&nonce[nonce_len - 4..nonce_len]);
+        let xor = m ^ seq;
+
+        // TODO: remove after debugging
+        dbg!(xor);
+
+        BigEndian::write_u32(&mut nonce[nonce_len - 4..nonce_len], xor);
+
+        nonce
     }
 }
 
@@ -64,28 +246,24 @@ impl Pipe for EceCrypt {
     type Writer = EceWriter;
 
     fn pipe(&mut self, input: &[u8]) -> (usize, Option<Vec<u8>>) {
-        // Match the chunk size
+        // Assert the chunk size
         assert_eq!(
-            input.len(),
+            input.len() as u32,
             self.chunk_size(),
             "input data passed to ECE cryptor does not match chunk size",
         );
 
-        // How much to read, based on capacity that is left and given bytes
-        let size = min(self.buf.capacity() - self.buf.len(), input.len());
-
-        // Read bytes into the buffer
-        self.buf.put(&input[0..size]);
-
-        // TODO: encrypt/decrypt bytes, produce the result
-        // panic!("not yet implemented");
-
-        dbg!(input.len());
+        // Encrypt or decrypt depending on the mode
+        let result = match self.mode {
+            CryptMode::Encrypt => self.pipe_encrypt(input),
+            CryptMode::Decrypt => self.pipe_decrypt(input),
+        };
 
         // Increase the sequence count
-        self.seq += 1;
+        self.seq  = self.seq.checked_add(1)
+            .expect("failed to crypt ECE payload, record sequence number exceeds limit");
 
-        self.pipe_transparent(input)
+        result
     }
 }
 
@@ -123,7 +301,7 @@ pub struct EceWriter {
 
 impl PipeRead<EceCrypt> for EceReader {
     fn new(crypt: EceCrypt, inner: Box<dyn Read>) -> Self {
-        let chunk_size = crypt.chunk_size();
+        let chunk_size = crypt.chunk_size() as usize;
 
         Self {
             crypt,
@@ -136,7 +314,7 @@ impl PipeRead<EceCrypt> for EceReader {
 
 impl PipeWrite<EceCrypt> for EceWriter {
     fn new(crypt: EceCrypt, inner: Box<dyn Write>) -> Self {
-        let chunk_size = crypt.chunk_size();
+        let chunk_size = crypt.chunk_size() as usize;
 
         Self {
             crypt,
@@ -166,7 +344,7 @@ impl Read for EceReader {
         }
 
         // Attempt to fill input buffer if has capacity upto the chunk size
-        let capacity = self.crypt.chunk_size() - self.buf_in.len();
+        let capacity = self.crypt.chunk_size() as usize - self.buf_in.len();
         if capacity > 0 {
             // Read from inner to input buffer
             let mut inner_buf = vec![0u8; capacity];
@@ -211,7 +389,7 @@ impl Read for EceReader {
 impl Write for EceWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         // Get the chunk size to use
-        let chunk_size = self.crypt.chunk_size();
+        let chunk_size = self.crypt.chunk_size() as usize;
 
         // Attempt to fill input buffer if has capacity upto the chunk size
         let capacity = chunk_size - self.buf.len();
