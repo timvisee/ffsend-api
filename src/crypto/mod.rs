d2255acd6ea039a9a8776ece2d5d45c0da6deb20
timvisee <tim@visee.me>
2019-03-13 20:29:56 +0100

    Implement ring backend for secure random byte generation

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -3,5 +3,13 @@ pub mod hkdf;
 pub mod key_set;
 pub mod sig;
 
-// Reexport the cryptographically secure random bytes generator
+// Re-export the cryptographically secure random bytes generator
+#[cfg(feature = "crypto-openssl")]
 pub use super::openssl::rand::rand_bytes;
+
+/// Cryptographically secure random bytes generator.
+#[cfg(feature = "crypto-ring")]
+pub fn rand_bytes(buf: &mut [u8]) -> Result<(), ring::error::Unspecified> {
+    use ring::{self, rand::{SecureRandom, SystemRandom}};
+    SystemRandom::new().fill(buf)
+}
