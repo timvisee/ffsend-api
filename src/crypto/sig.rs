ba9771645f0f0fdb6e447f66952eb6b7018e4fd5
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2019-03-15 14:24:04 +0100

    Implement ring backend for signing
    Based on github.com/vks/ffsend-api#d9da694e27eef1855fa8aa6267129a0c0b587b44
    
    Co-authored-by: Tim Visée <timvisee@gmail.com>
    
    See merge request timvisee/ffsend-api!6

--- src/crypto/sig.rs
+++ src/crypto/sig.rs
@@ -1,7 +1,12 @@
-use openssl::error::ErrorStack;
-use openssl::hash::MessageDigest;
-use openssl::pkey::PKey;
-use openssl::sign::Signer;
+#[cfg(feature = "crypto-openssl")]
+use openssl::{
+    error::ErrorStack,
+    hash::MessageDigest,
+    pkey::PKey,
+    sign::Signer,
+};
+#[cfg(feature = "crypto-ring")]
+use ring::{digest, hmac};
 
 use super::b64;
 
@@ -9,7 +14,8 @@ use super::b64;
 /// This is done using an HMAC key using the SHA256 digest.
 ///
 /// If computing the signature failed, an error is returned.
-pub fn signature(key: &[u8], data: &[u8]) -> Result<Vec<u8>, ErrorStack> {
+#[cfg(feature = "crypto-openssl")]
+fn signature_openssl(key: &[u8], data: &[u8]) -> Result<Vec<u8>, ErrorStack> {
     // Build the key, and signer
     let pkey = PKey::hmac(&key)?;
     let mut signer = Signer::new(MessageDigest::sha256(), &pkey)?;
@@ -24,9 +30,27 @@ pub fn signature(key: &[u8], data: &[u8]) -> Result<Vec<u8>, ErrorStack> {
 /// Compute the signature for the given data and key.
 /// This is done using an HMAC key using the SHA256 digest.
 ///
+/// If computing the signature failed, an error is returned.
+#[cfg(feature = "crypto-ring")]
+fn signature_ring(key: &[u8], data: &[u8]) -> Vec<u8> {
+    let skey = hmac::SigningKey::new(&digest::SHA256, key);
+    hmac::sign(&skey, data).as_ref().to_owned()
+}
+
+/// Compute the signature for the given data and key.
+/// This is done using an HMAC key using the SHA256 digest.
+///
 /// The resulting signature is encoded as base64 string in an URL-safe manner.
 ///
 /// If computing the signature failed, an error is returned.
-pub fn signature_encoded(key: &[u8], data: &[u8]) -> Result<String, ErrorStack> {
-    signature(key, data).map(|sig| b64::encode(&sig))
+pub fn signature_encoded(key: &[u8], data: &[u8]) -> Result<String, ()> {
+    #[cfg(feature = "crypto-openssl")]
+    {
+        signature_openssl(key, data).map(|sig| b64::encode(&sig)).map_err(|_| ())
+    }
+
+    #[cfg(feature = "crypto-ring")]
+    {
+        Ok(b64::encode(&signature_ring(key, data)))
+    }
 }
