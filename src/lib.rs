b0945933bf2c0b465e83f03912e6c270dca7a328
timvisee <tim@visee.me>
2020-04-27 15:29:47 +0200

    Remove obsolete implicit crate imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,16 @@
 #[macro_use]
 extern crate arrayref;
-extern crate byteorder;
-extern crate bytes;
 #[macro_use]
 extern crate derive_builder;
-extern crate failure;
 #[macro_use]
 extern crate failure_derive;
-extern crate mime_guess;
 // TODO: enable this attribute for selective compilation
 // #[cfg(feature = "crypto-openssl")]
 extern crate openssl;
 pub extern crate reqwest;
-#[cfg(feature = "crypto-ring")]
-extern crate ring;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
-extern crate time;
 pub extern crate url;
-extern crate version_compare;
 #[cfg(feature = "send3")]
 extern crate websocket;
 
