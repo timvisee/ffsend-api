29f1a817c51863bee416e428cdabfa0e417c2619
timvisee <tim@visee.me>
2020-05-17 12:41:24 +0200

    Only one crypto backend must be used, show warning through build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 fn main() {
+    // Require one crypto backend to be used
+    #[cfg(not(any(feature = "crypto-ring", feature = "crypto-openssl")))]
+    println!("cargo:warning=ffsend-api: must specify crypto backend, provide feature 'crypto-ring' or 'crypto-openssl'");
+    #[cfg(all(feature = "crypto-ring", feature = "crypto-openssl"))]
+    println!("cargo:warning=ffsend-api: feature 'crypto-ring' and 'crypto-openssl' cannot be used together");
+
     // crypto-ring and send2 cannot be used together
-    #[cfg(all(feature = "crypto-ring", feature = "send2",))]
+    #[cfg(all(feature = "crypto-ring", feature = "send2"))]
     {
         println!(
             "cargo:warning=ffsend-api: feature 'crypto-ring' and 'send2' cannot be used together"
